f8cca5091c4648fa2307ae54f79c9736a81a314b
Matias <matias.aleksandrowicz@gmail.com>
2025-01-29 14:35:22 -0300

    Added Recurrent Transactions Service

---
+++ src/main/java/com/myfinapp/service/RecurringTransactionsService.java
@@ -0,0 +1,40 @@
+package com.myfinapp.service;
+
+import com.myfinapp.model.RecurringTransactions;
+import com.myfinapp.repository.RecurringTransactionsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RecurringTransactionsService {
+    private final RecurringTransactionsRepository recurringTransactionsRepository;
+
+    public RecurringTransactionsService(RecurringTransactionsRepository recurringTransactionsRepository){
+        this.recurringTransactionsRepository = recurringTransactionsRepository;
+    }
+
+    public List<RecurringTransactions> getAllRecurringTransactions(){
+        return recurringTransactionsRepository.findAll();
+    }
+
+    public RecurringTransactions createRecurringTransactions(RecurringTransactions recurringTransactions){
+        return recurringTransactionsRepository.save(recurringTransactions);
+    }
+
+
+    //how to handle ids?
+    public RecurringTransactions updateRecurringTransactions(Long id, RecurringTransactions recurringTransactionsDetails){
+        return recurringTransactionsRepository.findById(id).map(recurringTransactions -> {
+            recurringTransactions.setCreated_date(recurringTransactionsDetails.getCreated_date());
+            recurringTransactions.setActive(recurringTransactionsDetails.isActive());
+            return recurringTransactionsRepository.save(recurringTransactions);
+        }).orElseThrow(()->new RuntimeException("Recurrent Transaction not found"));
+    }
+
+    public void deleteRecurringTransaction(Long id){
+        RecurringTransactions recurringTransactions = recurringTransactionsRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Recurrent Transaction not found"));
+        recurringTransactionsRepository.delete(recurringTransactions);
+    }
+}
