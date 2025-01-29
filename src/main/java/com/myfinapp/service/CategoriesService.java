4fc99212e1ad89c7579dbed81bdd27e947db7662
Matias <matias.aleksandrowicz@gmail.com>
2025-01-29 12:30:39 -0300

    Added CategoriesService

---
+++ src/main/java/com/myfinapp/service/CategoriesService.java
@@ -0,0 +1,37 @@
+package com.myfinapp.service;
+
+import com.myfinapp.model.Categories;
+import com.myfinapp.repository.CategoriesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriesService {
+    private final CategoriesRepository categoriesRepository;
+
+    public CategoriesService(CategoriesRepository categoriesRepository){
+        this.categoriesRepository = categoriesRepository;
+    }
+
+    public List<Categories> getAllCategories(){
+        return categoriesRepository.findAll();
+    }
+
+    public Categories createCategory(Categories categories){
+        return categoriesRepository.save(categories);
+    }
+
+    public Categories updateCategory(Long id, Categories categoriesDetails){
+        return categoriesRepository.findById(id).map(categories -> {
+            categories.setName(categoriesDetails.getName());
+            categories.setDate(categoriesDetails.getDate());
+            return categoriesRepository.save(categories);
+        }).orElseThrow(()-> new RuntimeException("Category not found"));
+    }
+
+    public void deleteCategory(Long id){
+        Categories categories = categoriesRepository.findById(id).orElseThrow(()-> new RuntimeException("Category not found"));
+        categoriesRepository.delete(categories);
+    }
+}
