45a55efa1b0ab2082ce2f68ba57fb2a894325970
Matias <matias.aleksandrowicz@gmail.com>
2025-02-13 15:57:29 -0300

    Deleted repo

--- src/main/java/com/myfinapp/repository/RecurringTransactionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.myfinapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecurringTransactionRepository extends JpaRepository<RecurringTransaction, Long> {
-}
